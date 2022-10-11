c6f8d9cb700f256e2aad56ca19d22d4ee8099d83
mihett05 <mihett05@yandex.ru>
2022-10-11 15:41:31 +0500

    Removed old files

--- public/models/db.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-const sqlite3 = require('sqlite3');
-
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'storage.db',
-});
-
-module.exports = sequelize;
--- public/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronApi', {
-  getIpcRenderer: () => ipcRenderer,
-  ipcOnce: (channel, cb) => ipcRenderer.once(channel, cb),
-});
